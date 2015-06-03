01b862dbaf0f0fd5359b39b31fed87eef0a826cb
Andrew Fink <andrewfink8@gmail.com>
2015-06-03 15:51:40 -0600

    got I1 done.... form-based workflow feature complete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+
+  include ArticlesHelper
+
   def index
     @articles = Article.all
   end
@@ -12,10 +15,14 @@ class ArticlesController < ApplicationController
   end
 
   def create
+
     @article = Article.new(article_params)
 
     if @article.save
-      redirect_to articles_path
+
+      flash.notice = "Article #{@article.title} was created!"
+
+      redirect_to article_path(@article)
     else
       flash[:erros] = "you gave me some bullshit...try again asshole"
       render :new
@@ -23,11 +30,30 @@ class ArticlesController < ApplicationController
 
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    flash.notice = "Article #{@article.title} was deleted!"
+
+    redirect_to articles_path
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    flash.notice = "Article #{@article.title} was updated!"
+
+    redirect_to article_path(@article)
+  end
+
   private
 
-  def article_params
-    params.require(:article).permit(:title, :body)
-  end
 
 
   # def set_song
